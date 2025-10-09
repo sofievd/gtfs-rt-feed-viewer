dd35543032bffbf76d5614f8eaf7712269b6cd85
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-10-09 17:38:19 +0200

    Added Font aweseome icons to style the arrows for the expandable tree.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faChevronDown} from '@fortawesome/free-solid-svg-icons'
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons'
+library.add(faChevronDown, faChevronRight)
+
+
 createApp(App).mount('#app')
