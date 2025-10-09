36405293362b4b714591e346b5f49bb2f66748fb
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-10-09 12:37:02 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div>
+    <GtfsViewer />
+  </div>
+
+</template>
+
+<script>
+import GtfsViewer from "./components/GtfsViewer.vue";
+export default {
+  name: "App",
+  components: {
+    GtfsViewer
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
